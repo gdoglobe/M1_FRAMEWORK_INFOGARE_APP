720fa1ae7eafd2da49445bf565a84c66c4d2f3fb
Arondefao <doglobegerard@gmail.com>
2020-11-21 00:43:56 +0100

    revert merge java2xsd to temp-sha_last_commit

--- project-jee8-model/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/jaxb/InfoCentreDto.java
+++ project-jee8-model/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/jaxb/InfoCentreDto.java
@@ -1,16 +1,15 @@
 package fr.pantheonsorbonne.ufr27.miage.model.jaxb;
 
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+
 import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
+
 import javax.xml.bind.annotation.XmlElement;
 
 
